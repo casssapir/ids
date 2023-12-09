b174c89a0c09b2385b412d48a40c6399f772f96d
Cass Sapir <1211799+casssapir@users.noreply.github.com>
2023-12-08 21:12:16 -0500

    save id fls in batches

--- ids.py
+++ ids.py
@@ -1,6 +1,7 @@
 import itertools
 import string
 import argparse
+import os
 
 def generate_unique_ids(char_count, num_ids=None):
     if not 1 <= char_count <= 16:
@@ -16,24 +17,38 @@ def generate_unique_ids(char_count, num_ids=None):
             raise ValueError("Number of IDs must be a positive number or 'all'. Given: {}".format(num_ids))
         return itertools.islice((''.join(combination) for combination in all_combinations), int(num_ids))
 
-def save_ids_to_file(ids, filename):
-    with open(filename, 'w') as file:
-        for id in ids:
-            file.write(id + '\n')
+def save_ids_to_file(ids, filename_prefix, max_ids_per_file=10000000):
+    file_number = 1
+    id_count = 0
+
+    file = open(f"{filename_prefix}_{file_number}.txt", 'w')
+
+    for id in ids:
+        file.write(id + '\n')
+        id_count += 1
+
+        if id_count >= max_ids_per_file:
+            file.close()
+            file_number += 1
+            id_count = 0
+            file = open(f"{filename_prefix}_{file_number}.txt", 'w')
+
+    file.close()
 
 def main():
     parser = argparse.ArgumentParser(description="Generate a list of unique IDs.")
     parser.add_argument("--char_count", type=int, default=3, help="Number of characters in each ID (between 1 and 16)")
-    parser.add_argument("--num_ids", default=1000, help="Number of IDs to generate or 'all' for no limit")
+    parser.add_argument("--num_ids", default='all', help="Number of IDs to generate or 'all' for no limit")
     args = parser.parse_args()
 
     # Convert num_ids to None if it's 'all'
-    num_ids = None if args.num_ids == 'all' else args.num_ids
+    num_ids = None if args.num_ids == 'all' else int(args.num_ids)
 
     unique_ids = generate_unique_ids(args.char_count, num_ids)
-    filename = 'ids.txt'
-    save_ids_to_file(unique_ids, filename)
-    print(f"Unique IDs have been saved to {filename}")
+    filename_prefix = 'ids'
+    save_ids_to_file(unique_ids, filename_prefix)
+
+    print(f"Unique IDs have been saved to {filename_prefix}_*.txt files")
 
 if __name__ == "__main__":
     main()
